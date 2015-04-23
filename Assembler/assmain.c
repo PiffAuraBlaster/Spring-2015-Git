f135df9f31df4a3606ed61e9fb208b740573c8a9
Kyle Napod <knnapod@puc.edu>
2015-04-23 10:28:08 -0700

    Moved Files Around

--- Assembler/assmain.c
+++ Assembler/assmain.c
@@ -12,16 +12,23 @@ int main()
 //Places to store strings for later use.//	
 	char listwords[100];
 	char objectwords[100];
+	char *line = NULL;
+	char read;
+	size_t len = 0;
 	
 //Open the input file if it exsists, otherwise gives an error message and quits//
+//Reads each line ignoring comment lines and prints results to screen and listing file//
 	assembly = fopen("lab.asm", "r");
 	if (assembly == NULL)
 		{perror("File does not exist");
 		exit(0);}
 	else {
+		while ((read = getline(&line, &len, assembly)) != -1) {
+			printf("%s", line);
+		}
 	}
 	
-//Opens the two output files and prints the header lines of the output files.//
+//Opens the two output files and Prints the header lines of the output files.//
 	listing = fopen("lab.lst","w");
 	if (listing == NULL) perror("File does not exist");
 	else {
