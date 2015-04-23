8314b40b157b907d9f0b94698e3e71f5c64b89fb
Kyle Napod <knnapod@puc.edu>
2015-04-23 11:36:15 -0700

    Changed Code

--- Assembler/functions.c
+++ Assembler/functions.c
@@ -5,32 +5,46 @@
 int getOpcode(char * command){
 	if 	(strcmp(command, "NOP") == 0);
 		return 0;
-	else (strcmp(command, "LDD") == 0);	 
+		
+	if (strcmp(command, "LDD") == 0);	 
 		return 1;
-	else (strcmp(command, "LDI") == 0);
+		
+	if (strcmp(command, "LDI") == 0);
 		return 2;
-	else (strcmp(command, "STO") == 0);
+		
+	if (strcmp(command, "STO") == 0);
 		return 3;
-	else (strcmp(command, "MOV") == 0);
+		
+	if (strcmp(command, "MOV") == 0);
 		return 4;
-	else (strcmp(command, "ADI") == 0);
+		
+	if (strcmp(command, "ADI") == 0);
 		return 5;
-	else (strcmp(command, "ADF") == 0);
+		
+	if (strcmp(command, "ADF") == 0);
 		return 6;
-	else (strcmp(command, "OR") == 0);
+		
+	if (strcmp(command, "OR") == 0);
 		return 7;
-	else (strcmp(command, "AND") == 0);
+		
+	if (strcmp(command, "AND") == 0);
 		return 8;
-	else (strcmp(command, "XOR") == 0);
+		
+	if (strcmp(command, "XOR") == 0);
 		return 9;
-	else (strcmp(command, "ROR") == 0);
+		
+	if (strcmp(command, "ROR") == 0);
 		return A;
-	else (strcmp(command, "JMP") == 0);
+		
+	if (strcmp(command, "JMP") == 0);
 		return B;
-	else (strcmp(command, "HLT") == 0);
+		
+	if (strcmp(command, "HLT") == 0);
 		return C;
-	else (strcmp(command, "LDX") == 0);
+		
+	if (strcmp(command, "LDX") == 0);
 		return D;
-	else (strcmp(command, "STX") == 0);
+		
+	if (strcmp(command, "STX") == 0);
 		return E;	
 	}
